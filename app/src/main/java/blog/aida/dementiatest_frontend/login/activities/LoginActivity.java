eff7f9ed97d476d9014dbb47897c1394df12ed73
aidadenisa <aida.denisa@yahoo.com>
2018-04-24 19:03:59 +0300

    [01-auth] Login

--- app/src/main/java/blog/aida/dementiatest_frontend/login/activities/LoginActivity.java
+++ app/src/main/java/blog/aida/dementiatest_frontend/login/activities/LoginActivity.java
@@ -1,25 +1,57 @@
 package blog.aida.dementiatest_frontend.login.activities;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import blog.aida.dementiatest_frontend.R;
+import blog.aida.dementiatest_frontend.login.requests.LoginRequest;
+import blog.aida.dementiatest_frontend.login.requests.RegisterRequest;
+import blog.aida.dementiatest_frontend.login.validators.DataValidator;
+import blog.aida.dementiatest_frontend.main.activities.models.UserAccount;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etEmail;
+    private EditText etPass;
+    private Button btnLogin;
+    private TextView registerLink;
+
+    private TextInputLayout emailWrapper;
+    private TextInputLayout passwordWrapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final EditText etEmail = findViewById(R.id.etEmail);
-        final EditText etPass = findViewById(R.id.etPass);
-        final TextView registerLink = findViewById(R.id.tvRegister);
-//        final Button btnRegister = findViewById(R.id.btnRegister);
+        etEmail = findViewById(R.id.etEmail);
+        etPass = findViewById(R.id.etPass);
+        registerLink = findViewById(R.id.tvRegister);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        emailWrapper = (TextInputLayout) findViewById(R.id.textInputEmail);
+        passwordWrapper = (TextInputLayout) findViewById(R.id.textInputPass);
 
         registerLink.setOnClickListener(new View.OnClickListener() {
 
@@ -29,5 +61,127 @@ public class LoginActivity extends AppCompatActivity {
                 LoginActivity.this.startActivity(registerIntent);
             }
         });
+
+        btnLogin.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                boolean preventRegistration = false;
+
+                if(!DataValidator.validateEmail(etEmail, emailWrapper)) {
+                    preventRegistration = true;
+                }
+
+                if(!validatePassword()) {
+                    preventRegistration = true;
+                }
+
+                if(preventRegistration) {
+                    return;
+                }
+
+                UserAccount newUser = LoginActivity.this.createUserAccountObject();
+
+                LoginActivity.this.loginUser(newUser);
+
+
+            }
+        });
     }
+
+    private void loginUser(UserAccount newUser) {
+
+        RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+
+        Map<String,String> loginData = new HashMap<>();
+        loginData.put("email", newUser.getEmail());
+        loginData.put("hash", newUser.getHash());
+
+        LoginRequest loginRequest = new LoginRequest(newUser, null, new Response.Listener<org.json.JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                JSONObject responseHeaders;
+
+                if(response != null) {
+                    try {
+                        responseHeaders = response.getJSONObject("headers");
+
+                        String token =  (responseHeaders.get("Authorization").toString().split(" "))[1];
+
+                        storeToken(token);
+
+
+
+
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("aida request response " + error.getMessage());
+                error.printStackTrace();
+                LoginActivity.this.createErrorAlertDialog(error);
+            }
+        });
+
+        queue.add(loginRequest);
+
+    }
+
+    private void createErrorAlertDialog(VolleyError error) {
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("Server Error");
+        alertDialog.setMessage("There was a server error and your request could not be processed. Please retry.");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+
+    private UserAccount createUserAccountObject() {
+
+        UserAccount userAccount = new UserAccount();
+        userAccount.setEmail(etEmail.getText().toString());
+        userAccount.setHash(etPass.getText().toString());
+
+        return userAccount;
+    }
+
+    private boolean validatePassword() {
+
+        String pass1 = etPass.getText().toString();
+
+        if(pass1.length() < 8) {
+           return false;
+        }
+
+        return true;
+    }
+
+    private void storeToken(String token) {
+        SharedPreferences settings = this.getSharedPreferences("userSettings", Context.MODE_PRIVATE);;
+        SharedPreferences.Editor edit = settings.edit();
+        edit.putString("token",token);
+        edit.apply();
+    }
+
+//    private String getToken() {
+//        SharedPreferences prefs = getSharedPreferences("userSettings",Context.MODE_PRIVATE);
+//        String tokennnn = prefs.getString("token","");
+//
+//        return tokennnn;
+//    }
 }
---
+++ app/src/main/java/blog/aida/dementiatest_frontend/login/requests/LoginRequest.java
@@ -0,0 +1,89 @@
+package blog.aida.dementiatest_frontend.login.requests;
+
+import android.support.annotation.NonNull;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Response;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import blog.aida.dementiatest_frontend.main.activities.models.UserAccount;
+
+/**
+ * Created by aida on 15-Apr-18.
+ */
+
+public class LoginRequest extends JsonObjectRequest {
+
+    private static final String LOGIN_URL = "http://10.11.31.5:8090/login";
+    private String body;
+    private Gson gson;
+
+    public LoginRequest(UserAccount userAccount, JSONObject jsonObject, Response.Listener<org.json.JSONObject> listener, Response.ErrorListener errorListener) {
+        super(Method.POST, LOGIN_URL, jsonObject , listener, errorListener);
+        gson = new GsonBuilder().create();
+        this.body = gson.toJson(userAccount);
+    }
+
+    @Override
+    public byte[] getBody() {
+        try {
+            return body == null ? null : body.getBytes("utf-8");
+        } catch (UnsupportedEncodingException uee) {
+            VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", body, "utf-8");
+            return null;
+        }
+    }
+
+    @Override
+    protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+        try {
+//            String jsonString = new String(response.data,
+//                    HttpHeaderParser.parseCharset(response.headers, PROTOCOL_CHARSET));
+
+            JSONObject jsonResponse = new JSONObject();
+//            jsonResponse.put("data", new JSONArray(jsonString));
+            jsonResponse.put("headers", new JSONObject(response.headers));
+
+            return Response.success(jsonResponse,
+                    HttpHeaderParser.parseCacheHeaders(response));
+        } catch (JSONException je) {
+            return Response.error(new ParseError(je));
+        }
+    }
+
+//    @Override
+//    public String getBodyContentType() {
+//        return "application/json; charset=utf-8";
+//    }
+//
+//    @Override
+//    public Map<String, String> getHeaders() throws AuthFailureError {
+//        Map<String, String> headers = super.getHeaders();
+//        int a = 2;
+//        return headers;
+//    }
+
+    @Override
+    public Map<String, String> getHeaders() throws AuthFailureError {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        return headers;
+    }
+
+}
